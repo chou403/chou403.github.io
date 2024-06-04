0ddb5db6e604ef896f8561cdcc43caf305bd9ab8
chouchou <jisy@yonyou.com>
2024-06-04 17:59:33 +0800

    test update date

---
+++ .husky/_/husky.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env sh
+if [ -z "$husky_skip_init" ]; then
+  debug () {
+    if [ "$HUSKY_DEBUG" = "1" ]; then
+      echo "husky (debug) - $1"
+    fi
+  }
+
+  readonly hook_name="$(basename -- "$0")"
+  debug "starting $hook_name..."
+
+  if [ "$HUSKY" = "0" ]; then
+    debug "HUSKY env variable is set to 0, skipping hook"
+    exit 0
+  fi
+
+  if [ -f ~/.huskyrc ]; then
+    debug "sourcing ~/.huskyrc"
+    . ~/.huskyrc
+  fi
+
+  readonly husky_skip_init=1
+  export husky_skip_init
+  sh -e "$0" "$@"
+  exitCode="$?"
+
+  if [ $exitCode != 0 ]; then
+    echo "husky - $hook_name hook exited with code $exitCode (error)"
+  fi
+
+  if [ $exitCode = 127 ]; then
+    echo "husky - command not found in PATH=$PATH"
+  fi
+
+  exit $exitCode
+fi
+
+git diff --cached --name-status |
+grep -i '^M.*\.mdx$' |
+while read _ file; do
+  filecontent=$(cat "$file")
+  frontmatter=$(echo "$filecontent" | awk -v RS='---' 'NR==2{print}')
+  draft=$(echo "$frontmatter" | awk '/^draft: /{print $2}')
+  if [ "$draft" = "false" ]; then
+    echo "$file updatedDate updated"
+    cat $file | sed "/---.*/,/---.*/s/^updatedDate:.*$/updatedDate: $(date "+%Y-%m-%dT%H:%M:%SZ")/" > tmp
+    mv tmp $file
+    git add $file
+  fi
+  if [ "$draft" = "first" ]; then
+    echo "First release of $file, draft set to false and updatedDate removed"
+    cat $file | sed "/---.*/,/---.*/s/^updatedDate:.*$/updatedDate:/" | sed "/---.*/,/---.*/s/^draft:.*$/draft: false/" > tmp
+    mv tmp $file
+    git add $file
+  fi
+done
