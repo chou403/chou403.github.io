9a849f57d279d99e05ad71bc5dcf7fb5302ada62
chou403 <chou403@users.noreply.github.com>
2024-09-04 08:28:46 +0000

    deploy: 1ba2b779826dff4c6d6e5f137b4a92db3795c908

---
+++ _astro/hoisted.AroCtZYD.js
@@ -0,0 +1,1 @@
+import{a as t}from"./hoisted.CHBExPSm.js";import"./hoisted.Do6zspqj.js";const o=localStorage.getItem("animations")==="true";o&&t(".title",{y:[80,0],opacity:[0,1]},{duration:2.5,opacity:{duration:3},offset:[0,.55,.75]});
