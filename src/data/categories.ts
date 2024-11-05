d44024a64c870f161fea43b7a4f06d8b5adcb407
chou403 <nuyoahchou@gmail.com>
2024-11-05 16:45:27 +0800

    add category html

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,2 @@
 // List of categories for blog posts
-export const CATEGORIES = ["Java", "Middleware", "Interview", "Tool", "Network", "Html"] as const;
+export const CATEGORIES = ["Java", "Middleware", "Interview", "Tool", "Network", "HTML"] as const;
