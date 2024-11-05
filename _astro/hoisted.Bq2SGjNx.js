9d65ca5d86b16b78f13d24def210b9bde803a09c
chou403 <chou403@users.noreply.github.com>
2024-11-05 08:47:22 +0000

    deploy: d44024a64c870f161fea43b7a4f06d8b5adcb407

---
+++ _astro/hoisted.Bq2SGjNx.js
@@ -0,0 +1,1 @@
+import{a as t}from"./hoisted.CsxoUaLs.js";const a=document.querySelectorAll("img");document.addEventListener("DOMContentLoaded",function(){localStorage.getItem("animations")==="true"?t(".effect",{opacity:[0,1],clipPath:["circle(0% at 0% 0%)","circle(100% at 50% 50%)"]},{duration:2,easing:"ease"}):a.forEach(e=>{e.classList.remove("opacity-0")})});
---
+++ _astro/hoisted.CniDvb6B.js
@@ -0,0 +1,1 @@
+import{a as t}from"./hoisted.CsxoUaLs.js";import"./hoisted.Bq2SGjNx.js";const o=localStorage.getItem("animations")==="true";o&&t(".title",{y:[80,0],opacity:[0,1]},{duration:2.5,opacity:{duration:3},offset:[0,.55,.75]});
